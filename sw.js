1483151f58208cbb629b52342477d00b43f8b1b1
AL <al@ALs-Mac-mini.local>
2026-02-02 13:19:06 -0500

    Initial deployment - STREAK game with PWA support

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Service Worker for STREAK PWA
+const CACHE_NAME = 'streak-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
